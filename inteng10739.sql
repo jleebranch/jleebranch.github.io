ce36189270c56dd25a6b45060c887da75b72f9f8
jleebranch <43584883+jleebranch@users.noreply.github.com>
2020-08-06 13:05:52 -0700

    Create inteng10739.sql

---
+++ inteng10739.sql
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta property="og:image" content="https://peptalkapp.com/img/logo@2xtest.png">
+    <meta name="branch:deeplink:$deeplink_path" content="tops://detail?sku=8856294057280">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>dummy page 2</title>
+
+    <script>
+	   
+	  
+
+	        (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking".split(" "), 0);
+
+	      
+    branch.init('key_live_fhu81HZ5sAUGmKxwuQgt6pcoFBnv1Ss8', options, function(err, data) {
+	      
+    });
+    
+	    branch.logEvent(
+     "COMPLETE_REGISTRATION",
+     { metadata: '123' },
+     function(err) { console.log(err); }
+);
+
+	    	    
+    </script>
+  </head>
+  <body>
+	  
+	  
+	  <a href="https://jleebranch.github.io/testpage2.html">https://jleebranch.github.io/testpage2.html</a>
+  <a href="https://branch.io/resources/aasa-validator/?domain=app.discover.com">https://branch.io/resources/aasa-validator/?domain=app.discover.com</a>
+	  
+	  
+	  
+	    <a href="intent://open?link_click_id=784209790063158960#Intent;scheme=rentomojo;package=com.rentomojo;S.browser_fallback_url=https%3A%2F%2Fwww.rentomojo.com%2F%3Futm_medium%3Dmarketing%26_branch_match_id%3D784209790063158960;S.market_referrer=link_click_id%3D784209790063158960%26utm_source%3DBranch%26utm_medium%3Dmarketing;S.branch_data=%7B%22%2Burl%22%3A%22https%3A%2F%2Frmojo.in%2FAyWpAnZO55%22%2C%22%24marketing_title%22%3A%22branch%20support%20test%202%22%2C%22~marketing%22%3Atrue%2C%22~creation_source%22%3A1%2C%22%24one_time_use%22%3Afalse%2C%22~id%22%3A%22784203092849297138%22%2C%22%24android_url%22%3A%22https%3A%2F%2Fwww.rentomojo.com%2F%22%2C%22~feature%22%3A%22marketing%22%2C%22%2Bdomain%22%3A%22ws3q.app.link%22%2C%22%2Bclick_timestamp%22%3A1588208410%2C%22%2Bclicked_branch_link%22%3Atrue%2C%22%2Bmatch_guaranteed%22%3Atrue%2C%22%2Bis_first_session%22%3Afalse%7D;B.branch_intent=true;end">intent://open?link_click_id=784209790063158960#Intent;scheme=rentomojo;package=com.rentomojo;S.browser_fallback_url=https%3A%2F%2Fwww.rentomojo.com%2F%3Futm_medium%3Dmarketing%26_branch_match_id%3D784209790063158960;S.market_referrer=link_click_id%3D784209790063158960%26utm_source%3DBranch%26utm_medium%3Dmarketing;S.branch_data=%7B%22%2Burl%22%3A%22https%3A%2F%2Frmojo.in%2FAyWpAnZO55%22%2C%22%24marketing_title%22%3A%22branch%20support%20test%202%22%2C%22~marketing%22%3Atrue%2C%22~creation_source%22%3A1%2C%22%24one_time_use%22%3Afalse%2C%22~id%22%3A%22784203092849297138%22%2C%22%24android_url%22%3A%22https%3A%2F%2Fwww.rentomojo.com%2F%22%2C%22~feature%22%3A%22marketing%22%2C%22%2Bdomain%22%3A%22ws3q.app.link%22%2C%22%2Bclick_timestamp%22%3A1588208410%2C%22%2Bclicked_branch_link%22%3Atrue%2C%22%2Bmatch_guaranteed%22%3Atrue%2C%22%2Bis_first_session%22%3Afalse%7D;B.branch_intent=true;end</a>
+	    
+	  <br>
+	  <a href="twitter://open?link_click_id=802304772634699490">twitter://open?link_click_id=802304772634699490</a>
+<br>
+
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
